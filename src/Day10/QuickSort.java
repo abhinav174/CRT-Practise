483af2ff48433729c49dde95ed1db98bdfca8854
G Abhinav <g.abhinav174@gmail.com>
2025-08-13 10:23:27 +0530

    Day 10 Commit

--- src/Day10/QuickSort.java
+++ src/Day10/QuickSort.java
@@ -2,26 +2,38 @@ package Day10;
 
 public class QuickSort {
     public static void quickSort(int[] arr, int low, int high){
-        if(low<high) {
+        if(low < high) {
             int pivot = partition(arr, low, high);
             quickSort(arr, low, pivot - 1);
             quickSort(arr, pivot + 1, high);
         }
     }
 
-    public static int partition(int [] arr, int low, int high){
+    public static int partition(int[] arr, int low, int high){
         int pivot = arr[high];
-        int i = low -1;
-        for (int j = 0; j < high; j++) {
-            if(arr[j]<= pivot){
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if(arr[j] <= pivot){
                 i++;
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
-        int temp = arr[i+1];
-        arr[i+1] = arr[high];
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
         arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 8, 6, 7, 9, 15};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("Sorted Array:");
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
     }
 }
+
